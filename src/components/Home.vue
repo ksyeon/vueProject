0dc3f97d568f6adcfaa37709656efb18626a9064
kimsuyeon <kimsuyeon@saferzone.kr>
2019-01-09 18:02:32 +0900

    home.vue 수정

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,8 @@
     <div id="home">
         <h1>Photos</h1>
 
-        <div class="">
-            <div class=""></div>
+        <div class="div-content">
+            <div class="div1"></div>
             <div>설명 설명 설명</div>
         </div>
     </div>
@@ -11,6 +11,6 @@
 
 <script>
 export default {
-    name: 'Home'
+  name: 'Home'
 }
 </script>
